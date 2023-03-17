4f476541d3a5c5eb86281062c8e5c2273de7af9f
minhnguyen0910 <minhn091099@gmail.com>
2023-03-17 09:54:26 +0700

    MinhNV up

--- src/main/java/com/example/bookstores/model/Customer.java
+++ src/main/java/com/example/bookstores/model/Customer.java
@@ -6,10 +6,57 @@ public class Customer {
     String name;
     Date dateOfBirth;
     String email;
-    Byte gender;
+    int gender;
     String nameAccount;
 
     public Customer() {
     }
 
+    public Customer(String name, Date dateOfBirth, String email, int gender, String nameAccount) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.gender = gender;
+        this.nameAccount = nameAccount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getNameAccount() {
+        return nameAccount;
+    }
+
+    public void setNameAccount(String nameAccount) {
+        this.nameAccount = nameAccount;
+    }
 }
